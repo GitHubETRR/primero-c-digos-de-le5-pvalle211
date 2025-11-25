25fd99c03a514e8058b5901072d9bcd440cfd13a
pvalle211 <pablovalleextras@gmail.com>
2025-11-25 20:31:47 -0300

    Add files via upload

---
+++ CodigosLE5/Strings1PV.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int Dimension(char cad[]);
+
+int main(){
+    char texto[100];
+    int largo;
+
+    printf("Ingrese un texto: ");
+    scanf("%s",texto);    // cuenta hasta el primer espacio :(
+
+    largo=Dimension(texto);
+
+    printf("El largo del texto es: %d\n",largo);
+
+    return 0;
+}
+
+int Dimension(char cad[]){
+    return strlen(cad);
+}
